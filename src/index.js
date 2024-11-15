5ee49828a87b220eecdd66438f9ea270670c8f4a
NureTokarDenys <denys.tokar@nure.ua>
2024-11-15 16:41:14 +0200

    fe init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const express = require('express')
 
 const app = express();
@@ -11,3 +12,15 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Running on port ${PORT}`);
 }); 
+=======
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+>>>>>>> d49897f (fe init)
