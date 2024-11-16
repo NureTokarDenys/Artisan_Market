21e545f3a25537310bb7e2566640084aa182ff34
NureTokarDenys <denys.tokar@nure.ua>
2024-11-16 21:43:44 +0200

    register and login pop ups init

---
+++ src/LoginPopUp.js
@@ -0,0 +1,9 @@
+import './LoginPopUp.css';
+
+const LoginPopUp = () => {
+  return (
+    <div>LoginPopUp</div>
+  )
+}
+
+export default LoginPopUp
---
+++ src/RegisterPopUp.js
@@ -0,0 +1,9 @@
+import './RegisterPopUp.css';
+
+const RegisterPopUp = () => {
+  return (
+    <div>RegisterPopUp</div>
+  )
+}
+
+export default RegisterPopUp
