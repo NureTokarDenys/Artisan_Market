dee64c76d76c9297cd9dfac9aa9fbd75e53b888e
NureTokarDenys <denys.tokar@nure.ua>
2024-11-18 20:43:33 +0200

    added "Error 404" page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,8 +1,16 @@
+import { Link } from 'react-router-dom';
 import './NotFound.css';
 
 const NotFound = () => {
   return (
-    <main>NotFound</main>
+    <div className='error-wrapper'>
+      <div class="error-container">
+          <div class="error-code">404</div>
+          <div class="error-message">Oops! Page Not Found</div>
+          <p>The page you are looking for is nonexistent or might have been removed, had its name changed, or is temporarily unavailable.</p>
+          <Link to="/products" class="return-link">Return to Homepage</Link>
+      </div>
+    </div>
   )
 }
 
