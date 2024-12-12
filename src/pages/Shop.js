5451f7b6eca32effd9ea576571ac300e3d402855
NureTokarDenys <denys.tokar@nure.ua>
2024-12-12 12:28:01 +0200

    update no products shop logic

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -3,6 +3,17 @@ import Sorting from "../components/Sorting";
 import "./Shop.css";
 
 const Shop = ({ products, sortOptions, sort, setSort, wishlist, setWishlist }) => {
+
+  const isEmpty = (products?.length || 0) === 0;
+  if(isEmpty){
+    return (
+      <main className='empty-shop'>
+        <h1 className='empty-shop-header'>There is an error loading products</h1>
+        <h3 className='empty-shop-text'>Please try again later</h3>
+      </main>
+    );
+  }
+
   return (
     <main className="shop">
       <Sorting h1={"Explore Handmade Items"} h2={"Browse our collection of handcrafted, one-of-a-kind products made by artisans"} sortOptions={sortOptions} sort={sort} setSort={setSort} />
